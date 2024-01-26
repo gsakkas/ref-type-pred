580202007d7fd27de79219fb0e8da9ce489fddcd
George Sakkas <george.p.sakkas@gmail.com>
2024-01-25 23:14:05 -0800

    Basic setup script to run LLM generation

---
+++ src/basic_setup.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/gsakkas/ref-type-pred.git
+cd ref-type-pred
+sudo apt-get -y install vim nvidia-cuda-toolkit
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install --upgrade huggingface_hub
+pip install -r src/requirements.txt
+vim get_starcoder_code_suggestions.py
+export OPENAI_KEY="dummy"
+huggingface-cli login --token dummy
+CUDA_VISIBLE_DEVICES=0 python run_llm_generation.py --llm starcoderbase-3B --data_file lh_test_set_final_raw_v3.jsonl --total_repairs 20 --cache_file lh_starcoderbase_3B_finetuned_with_the_stack_chkpnt_20000_cache_raw_v3.json --create_cache_only
