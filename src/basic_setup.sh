22371dfb53f7bd43b328acf34ee6c6d1b58d7a8f
George Sakkas <george.p.sakkas@gmail.com>
2024-02-01 02:10:52 -0800

    Updated basic installation script

--- src/basic_setup.sh
+++ src/basic_setup.sh
@@ -8,5 +8,6 @@ pip install --upgrade huggingface_hub
 pip install -r src/requirements.txt
 vim get_starcoder_code_suggestions.py
 export OPENAI_KEY="dummy"
+export PYTHONPATH=$PYTHONPATH:/home/gsakkas/Documents/UCSD/Program-Analytics/ref-type-pred/src
 huggingface-cli login --token dummy # TODO: change dummy with actual token
 CUDA_VISIBLE_DEVICES=0 python run_llm_generation.py --llm starcoderbase-3B --data_file lh_test_set_final_raw_v3.jsonl --total_repairs 20 --cache_file lh_starcoderbase_3B_finetuned_with_the_stack_chkpnt_20000_cache_raw_v3.json --create_cache_only
